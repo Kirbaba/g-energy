f87154f1f0c642dea38088b231a5974e1821f5f8
Vladislav <v.kirbaba@gmail.com>
2016-03-25 16:12:00 +0300

    Карусель новостей

--- app/js/script.js
+++ app/js/script.js
@@ -18,10 +18,110 @@ $(document).ready(function() {
       // itemsMobile : false
  
   });
+
+
+ 
+   $(".home-news__tab--news--slider").owlCarousel({        
+      navigation : false, 
+      items : 4,
+      slideSpeed : 1000,
+      paginationSpeed : 400,
+      touchDrag: true,
+      autoplayHoverPause: true,
+      items: [4],
+      itemsDesktop: [1280, 3],
+      itemsDesktopSmall: [1000, 2],
+      itemsMobile: [690, 1],
+      navigation: true,
+      navigationText : false,
+      pagination : false
+
+  });
+
+ 
+   $(".home-news__tab--articles--slider").owlCarousel({        
+      navigation : false, 
+      items : 4,
+      slideSpeed : 1000,
+      paginationSpeed : 400,
+      touchDrag: true,
+      autoplayHoverPause: true,
+      items: [4],
+      itemsDesktop: [1280, 3],
+      itemsDesktopSmall: [1000, 2],
+      itemsMobile: [690, 1],
+      navigation: true,
+      navigationText : false,
+      pagination : false
+
+  });
+
+ 
+   $(".home-news__tab--video--slider").owlCarousel({        
+      navigation : false, 
+      items : 4,
+      slideSpeed : 1000,
+      paginationSpeed : 400,
+      touchDrag: true,
+      autoplayHoverPause: true,
+      items: [4],
+      itemsDesktop: [1280, 3],
+      itemsDesktopSmall: [1000, 2],
+      itemsMobile: [690, 1],
+      navigation: true,
+      navigationText : false,
+      pagination : false
+
+  });
+
+
  
 });
 
 $(function() {
     $('.top-slider__item h2').highlight( 'G-' );
+    
+});
+
+$(function() {
+    /*jQuery('#home-news__tab--news').slick({
+        dots: false,
+        infinite: true,
+        autoplay: false,
+        adaptiveHeight: true,
+        speed: 300,
+        arrows: true,        
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });*/
+
     
 });
